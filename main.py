226961acca73a6b89aa3d9d850641e1b7b6fc4cd
Hochu spat <liza10102008@mail.ru>
2025-06-03 17:05:21 +0300

    Пересборка

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from models import get_db, Base, engine
 from repositories import UserRepository, SatelliteRepository
 from services import UserService, SatelliteService
 from controllers import UserController, SatelliteController
+from core import GeometryEngine
 from routers import UserRouter, SatelliteRouter
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -12,10 +13,12 @@ if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     load_dotenv(override=True)
     with get_db() as db:
+        g_e = GeometryEngine()
+
         user_repo = UserRepository(db)
         satellite_repo = SatelliteRepository(db)
         user_service = UserService(user_repo)
-        satellite_service = SatelliteService(satellite_repo)
+        satellite_service = SatelliteService(satellite_repo, g_e)
         user_controller = UserController(user_service)
         satellite_controller = SatelliteController(satellite_service)
         user_router = UserRouter(user_controller)

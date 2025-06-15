eae295176ab332042dbb5574f3c1604568c4c881
cos-sinus <As10819i@yandex.ru>
2025-06-15 17:21:24 +0300

    + фикс времени

--- core/geometry_engine.py
+++ core/geometry_engine.py
@@ -4,17 +4,17 @@ import numpy as np
 
 
 class GeometryEngine:
-    def check_visibility(self, target_schema: SatelliteSchema, observer_schema: SatelliteSchema, d:int=100) -> Time | None:
+    def check_visibility(self, target_schema: SatelliteSchema, observer_schema: SatelliteSchema, d:int=50) -> Time | None:
         ts = load.timescale()
-
+        count = 0
         target_tle = target_schema.full_TLE.splitlines()
         observer_tle = observer_schema.full_TLE.splitlines()
         target = EarthSatellite(*target_tle, name=target_schema.name, ts =ts)
         observer = EarthSatellite(*observer_tle, name=observer_schema.name, ts=ts)
-
+    
         # Временной диапазон (ближайшие 24 часа)
         t_start = ts.now()
-        t_end = t_start + 1  # 1 день
+        t_end = t_start + 2  # 1 день
         times = ts.linspace(t_start, t_end, 1440)  # Шаг 1 минута
 
         # Проверка условий
